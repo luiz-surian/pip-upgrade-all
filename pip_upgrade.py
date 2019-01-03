7252fd451c1fd049cac6679459f3425f4120b947
Luiz Fernando Surian Filho <lfsurianfilho@gmail.com>
2019-01-03 13:38:16 -0200

    Update pip_upgrade.py

--- pip_upgrade.py
+++ pip_upgrade.py
@@ -2,10 +2,8 @@ import pkg_resources
 
 packages = ' '.join([dist.project_name for dist in pkg_resources.working_set])
 
-
 with open('pip_upgrade.bat', 'w') as bat_file:
     bat_file.write(
-        '@echo off\n'
         f'pip install --upgrade {packages}\n'
         'pause'
     )
